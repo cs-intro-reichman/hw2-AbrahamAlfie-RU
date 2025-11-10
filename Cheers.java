0a4a0a10141183336510bd999dd67012113edc8d
Abraham Alfie <abraham.alfiesalmun@post.runi.ac.il>
2025-11-10 08:49:53 +0200

    Completed Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -4,10 +4,22 @@
 Great work boaz!
 */
 
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0].toUpperCase();
+                int nTimes = Integer.parseInt(args[1]);
+                String anLetters = "AEFHILMNORSX";
+
+                for (int i = 0; i < word.length(); i++) {
+                        char currentLetter = word.charAt(i);
+                        int indexOfAn = anLetters.indexOf(currentLetter);
+                        String aOrAn = (indexOfAn != -1) ? "an" : "a ";
+                        System.out.println("Give me " + aOrAn + " " + currentLetter + ": " + currentLetter + "!");
+                }
+
+                for (int i = 0; i < nTimes; i++) {
+                        System.out.println(word + "!!!");
+                }
         }
 }
